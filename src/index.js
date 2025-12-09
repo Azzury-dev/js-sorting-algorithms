39dfc90857b01eab9673ab403fad65e554a633f9
azzury <rodaucourt08@gmail.com>
2025-12-09 16:26:25 +0100

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
 // Basic algorithms
 export * from "./basic/bubble.js";
-export * from "./basic/selection.js";
-export * from "./basic/insertion.js";
-export * from "./basic/shell.js";
 
 // Advanced algorithms
-export * from "./advanced/quick.js";
-export * from "./advanced/merge.js";
-export * from "./advanced/heap.js";
-export * from "./advanced/counting.js";
-export * from "./advanced/radix.js";
-export * from "./advanced/bucket.js";
 
 // Utils
 export * from "./utils/swap.js";
