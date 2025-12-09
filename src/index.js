0dfcf7ac5c027a5c9332c1ae984d35ff26e17b8f
azzury <rodaucourt08@gmail.com>
2025-12-09 16:25:47 +0100

    Added the src exporter

---
+++ src/index.js
@@ -0,0 +1,18 @@
+// Basic algorithms
+export * from "./basic/bubble.js";
+export * from "./basic/selection.js";
+export * from "./basic/insertion.js";
+export * from "./basic/shell.js";
+
+// Advanced algorithms
+export * from "./advanced/quick.js";
+export * from "./advanced/merge.js";
+export * from "./advanced/heap.js";
+export * from "./advanced/counting.js";
+export * from "./advanced/radix.js";
+export * from "./advanced/bucket.js";
+
+// Utils
+export * from "./utils/swap.js";
+export * from "./utils/generateArray.js";
+export * from "./utils/isSorted.js";
